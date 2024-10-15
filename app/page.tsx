7d9f6df6539b192b84f3f6bf96eaa3073047644c
mogi86 <nobuyuki.mogi@gmail.com>
2024-10-15 13:57:36 +0900

    raise error when use query parameter error_key

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const paramValue = searchParams.error_key || "defaultValue";
+
+  const shouldError = paramValue === "true";
+
+  if (shouldError) {
+    throw new Error("Intentional Error");
+  }
   return (
     <div>
       <Link href="/dashboard">Dashboard</Link>
