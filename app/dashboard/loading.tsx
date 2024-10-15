8d83e17ed5e4323175e920e4f91657775dd8b0f9
mogi86 <nobuyuki.mogi@gmail.com>
2024-10-15 13:56:45 +0900

    add loadding

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+// LoadingSkeleton.tsx
+export default function LoadingSkeleton() {
+  return (
+    <div style={{ width: "100%", height: "100px", backgroundColor: "#e0e0e0" }}>
+      {/* スケルトン用のUIを追加 */}
+    </div>
+  );
+}
