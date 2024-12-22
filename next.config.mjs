f179b666946336099ba65cd597683a3632d152d8
mogi86 <nobuyuki.mogi@gmail.com>
2024-12-22 22:44:46 +0900

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // 静的ファイルの生成を有効化
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // 画像最適化を無効化（静的ファイル向け）
+  },
+};
 
 export default nextConfig;
